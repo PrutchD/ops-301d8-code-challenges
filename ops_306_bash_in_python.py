e48323c26790af493f74440f8f7976aca15f7fdc
Dave Prutch <prutch.david@gmail.com>
2023-06-07 00:09:04 +0000

    update ops 301 challenge 6

--- ops_306_bash_in_python.py
+++ ops_306_bash_in_python.py
@@ -6,8 +6,6 @@
 # Purpose:                      Execute Linux terminal commands from a Python script
 
 # Import Libraries
-# Import the OS to utilize Bash commands
-import os
 # Import the subprocess module
 import subprocess
 
@@ -15,23 +13,19 @@ import subprocess
 # Creates variables which are set to output of respective bash command. 
 # os-system command invokes the OS to run the command.
 # Gets user name
-user = os.system("whoami")
-user1 = subprocess.run("whoami", capture_output=True)
+user = subprocess.run("whoami", capture_output=True)
 # Gets IP address data
-ip = os.system("ip a")
-ip1 = subprocess.run(["ip", "a"], capture_output=True)
+ip = subprocess.run(["ip", "a"], capture_output=True)
 # Gets system information
-short_info = os.system("sudo lshw -short")
-short_info1 = subprocess.run(["sudo", "lshw", "-short"], capture_output=True)
+short_info = subprocess.run(["sudo", "lshw", "-short"], capture_output=True)
 # Declaration of functions
 
 
 # Main
 # Print each variable to the terminal usint print() function
-print (user)
-print (ip)
-print (short_info)
-print (user1)
-print (ip1)
-print (short_info1)
+# translate stdout to string
+
+print (user.stdout.decode('utf=8'))
+print (ip.stdout.decode('utf=8'))
+print (short_info.stdout.decode('utf=8'))
 # End
